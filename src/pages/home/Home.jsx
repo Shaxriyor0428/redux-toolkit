02bb06519d8143b81c8f6643b5166785237ba3e3
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-12-09 20:01:01 +0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-[70vh]">
+      <p className="text-lg text-slate-700 container mt-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quas
+        labore, optio ullam alias quasi eum, ab quibusdam placeat minus suscipit
+        sed. Suscipit repellendus magnam neque necessitatibus? Possimus
+        asperiores incidunt explicabo hic. Quo harum, sint, facere libero
+        voluptas aut nobis maxime obcaecati est unde odio illum laboriosam
+        temporibus sunt modi placeat ea natus nisi. Neque facilis dolorum quo
+        mollitia omnis excepturi consequuntur pariatur quae eum distinctio
+        suscipit, explicabo corporis asperiores nemo eius modi. Eveniet autem
+        est sequi corporis cupiditate amet sint earum rerum fugiat aliquid ipsam
+        nam non blanditiis officia totam quo magnam doloremque, hic laboriosam
+        omnis? Et, expedita id!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
